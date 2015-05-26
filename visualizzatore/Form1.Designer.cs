5e4866b71029b6b1a6388449ed4d635396aa59ad
alessandro.stanga@me.com <alessandro.stanga@me.com>
2015-05-26 17:49:14 +0200

    Initial commit

---
+++ visualizzatore/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace visualizzatore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textRicerca = new System.Windows.Forms.TextBox();
+            this.pictureImaggine = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dwgbox = new AxACCTRLLib.AxAcCtrl();
+            this.loadingPicture = new System.Windows.Forms.PictureBox();
+            this.labelerrore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureImaggine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dwgbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textRicerca
+            // 
+            this.textRicerca.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textRicerca.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textRicerca.Location = new System.Drawing.Point(0, 0);
+            this.textRicerca.Name = "textRicerca";
+            this.textRicerca.Size = new System.Drawing.Size(1082, 47);
+            this.textRicerca.TabIndex = 4;
+            this.textRicerca.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textRicerca.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textRicerca_KeyPress_1);
+            // 
+            // pictureImaggine
+            // 
+            this.pictureImaggine.Location = new System.Drawing.Point(357, 57);
+            this.pictureImaggine.Name = "pictureImaggine";
+            this.pictureImaggine.Size = new System.Drawing.Size(312, 206);
+            this.pictureImaggine.TabIndex = 5;
+            this.pictureImaggine.TabStop = false;
+            this.pictureImaggine.Visible = false;
+            // 
+            // dwgbox
+            // 
+            this.dwgbox.Enabled = true;
+            this.dwgbox.Location = new System.Drawing.Point(22, 57);
+            this.dwgbox.Name = "dwgbox";
+            this.dwgbox.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("dwgbox.OcxState")));
+            this.dwgbox.Size = new System.Drawing.Size(329, 206);
+            this.dwgbox.TabIndex = 6;
+            this.dwgbox.Visible = false;
+            // 
+            // loadingPicture
+            // 
+            this.loadingPicture.Location = new System.Drawing.Point(22, 269);
+            this.loadingPicture.Name = "loadingPicture";
+            this.loadingPicture.Size = new System.Drawing.Size(426, 217);
+            this.loadingPicture.TabIndex = 7;
+            this.loadingPicture.TabStop = false;
+            this.loadingPicture.Visible = false;
+            // 
+            // labelerrore
+            // 
+            this.labelerrore.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelerrore.ForeColor = System.Drawing.Color.Red;
+            this.labelerrore.Location = new System.Drawing.Point(308, 328);
+            this.labelerrore.Name = "labelerrore";
+            this.labelerrore.Size = new System.Drawing.Size(662, 71);
+            this.labelerrore.TabIndex = 8;
+            this.labelerrore.Text = "CODICE NON TROVATO";
+            this.labelerrore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelerrore.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 757);
+            this.Controls.Add(this.labelerrore);
+            this.Controls.Add(this.loadingPicture);
+            this.Controls.Add(this.dwgbox);
+            this.Controls.Add(this.pictureImaggine);
+            this.Controls.Add(this.textRicerca);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visualizzatore";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureImaggine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dwgbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textRicerca;
+        private System.Windows.Forms.PictureBox pictureImaggine;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private AxACCTRLLib.AxAcCtrl dwgbox;
+        private System.Windows.Forms.PictureBox loadingPicture;
+        private System.Windows.Forms.Label labelerrore;
+
+    }
+}
+
---
+++ visualizzatore/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace visualizzatore
+{
+    public partial class Form1 : Form
+    {
+        public string[] percorsi;
+        public Form1()
+        {
+            InitializeComponent();
+            loadingPicture.ImageLocation = Application.StartupPath + @"\loading.gif";
+        }
+
+        private void textRicerca_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                pictureImaggine.Image = null;
+                pictureImaggine.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureImaggine.Visible = false;
+                loadingPicture.SizeMode = PictureBoxSizeMode.Zoom;
+                loadingPicture.Visible = true;
+                loadingPicture.Dock = DockStyle.Fill;
+                labelerrore.Dock = DockStyle.Fill;
+                Application.DoEvents();
+                bool trovato = false;
+                labelerrore.Visible = false;
+                foreach (string percorso in percorsi)
+                {
+                    string[] files = Directory.GetFiles(percorso, "*.*", SearchOption.AllDirectories).OrderByDescending(d => new FileInfo(d).LastWriteTime).ToArray();
+                    foreach (string file in files)
+                    {
+                        if (file.Contains(textRicerca.Text) && textRicerca.Text != "")
+                        {
+                            if (file.EndsWith("jpg") || file.EndsWith("bmp") || file.EndsWith("png"))
+                            {
+                                trovato = true;
+                                pictureImaggine.Visible = true;
+                                pictureImaggine.Dock = DockStyle.Fill;
+                                dwgbox.Visible = false;
+                                dwgbox.Dock = DockStyle.None;
+                                pictureImaggine.ImageLocation = file;
+                            }
+                            if (file.EndsWith("dwg") || file.EndsWith("dxf"))
+                            {
+                                trovato = true;
+                                pictureImaggine.Visible = false;
+                                pictureImaggine.Dock = DockStyle.None;
+                                dwgbox.Visible = true;
+                                dwgbox.Dock = DockStyle.Fill;
+                                dwgbox.PutSourcePath(file);
+                                Application.DoEvents();
+                            }
+                            break;
+                        }
+                    }
+                    loadingPicture.Visible = false;
+                    loadingPicture.Dock = DockStyle.None;
+                    labelerrore.Visible = !trovato;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dwgbox._Deactivate();
+            dwgbox = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string config_file = Application.StartupPath + @"\config.ini";
+            if (File.Exists(config_file))//controlla se esiste il file di configurazione 
+            {
+                int ind = 0;
+                string[] lines = System.IO.File.ReadAllLines(config_file);//se c'è lo legge 
+                percorsi = lines;
+            }
+
+            else
+            {//se non trova il file di configurazione 
+                MessageBox.Show("Non trovo il file di configurazione: " + config_file);
+                textRicerca.Enabled = false;
+                textRicerca.Text = "Impostare il file di configurazione";
+            }
+        }
+    }
+ }
